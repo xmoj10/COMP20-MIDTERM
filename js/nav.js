0b52e26f36d699921569dff31698cf48e710f29e
Trang Hoang <tychuotsieu45@gmail.com>
2020-03-02 20:51:55 -0500

    .

--- js/nav.js
+++ js/nav.js
@@ -9,7 +9,7 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = '';
       } else {
-        link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 12 + 0.1}s';
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 12 + 0.1}s`;
       }
     });
 
